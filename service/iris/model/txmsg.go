bc50d1617c12273006b41da782a805963780039c
黄 伟昌 <weichang@bianjie.ai>
2019-08-22 15:43:24 +0800

    fix the code about save msgtype include SubmitProposal

--- service/iris/model/txmsg.go
+++ service/iris/model/txmsg.go
@@ -1,8 +1,30 @@
 package iris
 
-import "github.com/irisnet/rainbow-sync/service/iris/constant"
+import (
+	"github.com/irisnet/rainbow-sync/service/iris/constant"
+	"github.com/irisnet/irishub/app/v1/gov"
+	"strconv"
+)
+
+type Param struct {
+	Subspace string `bson:"subspace"`
+	Key      string `bson:"key"`
+	Value    string `bson:"value"`
+}
+
+type Params []Param
+
+type DocTxMsgSubmitProposal struct {
+	Title          string `bson:"title"`          //  Title of the proposal
+	Description    string `bson:"description"`    //  Description of the proposal
+	Proposer       string `bson:"proposer"`       //  Address of the proposer
+	InitialDeposit Coins  `bson:"initialDeposit"` //  Initial deposit paid by sender. Must be strictly positive.
+	ProposalType   string `bson:"proposalType"`   //  Initial deposit paid by sender. Must be strictly positive.
+	Params         Params `bson:"params"`
+}
 
 type DocTxMsgSubmitTokenAdditionProposal struct {
+	DocTxMsgSubmitProposal
 	Symbol          string `bson:"symbol"`
 	CanonicalSymbol string `bson:"canonical_symbol"`
 	Name            string `bson:"name"`
@@ -17,12 +39,33 @@ func (doctx *DocTxMsgSubmitTokenAdditionProposal) Type() string {
 
 func (doctx *DocTxMsgSubmitTokenAdditionProposal) BuildMsg(txMsg interface{}) {
 	msg := txMsg.(MsgSubmitTokenAdditionProposal)
+	doctx.Title = msg.Title
+	doctx.Description = msg.Description
+	doctx.ProposalType = msg.ProposalType.String()
+	doctx.Proposer = msg.Proposer.String()
+	doctx.Params = loadParams(msg.Params)
+	doctx.InitialDeposit = loadInitialDeposit(msg.InitialDeposit)
 	doctx.Symbol = msg.Symbol
 	doctx.MinUnitAlias = msg.MinUnitAlias
 	doctx.CanonicalSymbol = msg.CanonicalSymbol
 	doctx.Name = msg.Name
 	doctx.Decimal = msg.Decimal
 	doctx.InitialSupply = msg.InitialSupply
+}
+
+func loadParams(params []gov.Param) (result []Param) {
+	for _, val := range params {
+		result = append(result, Param{Subspace: val.Subspace, Value: val.Value, Key: val.Key})
+	}
+	return
+}
+
+func loadInitialDeposit(coins SdkCoins) (result Coins) {
+	for _, val := range coins {
+		amt, _ := strconv.ParseFloat(val.Amount.String(), 64)
+		result = append(result, &Coin{Amount: amt, Denom: val.Denom})
+	}
+	return
 }
 
 type DocTxMsgSetMemoRegexp struct {
