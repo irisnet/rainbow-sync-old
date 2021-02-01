9b18f668ea3814580d63ce5f9f68616b61e1845a
huangweichang <weichang@bianjie.ai>
2021-02-01 19:24:08 +0800

    fix option type

--- msgs/gov/gov.go
+++ msgs/gov/gov.go
@@ -75,7 +75,7 @@ func (m *DocTxMsgSubmitProposal) HandleTxMsg(v SdkMsg) MsgDocInfo {
 type DocTxMsgVote struct {
 	ProposalID uint64 `bson:"proposal_id"` // ID of the proposal
 	Voter      string `bson:"voter"`       //  address of the voter
-	Option     string `bson:"option"`      //  option from OptionSet chosen by the voter
+	Option     int32  `bson:"option"`      //  option from OptionSet chosen by the voter
 }
 
 func (doctx *DocTxMsgVote) GetType() string {
@@ -85,7 +85,7 @@ func (doctx *DocTxMsgVote) GetType() string {
 func (doctx *DocTxMsgVote) BuildMsg(txMsg interface{}) {
 	msg := txMsg.(*MsgVote)
 	doctx.Voter = msg.Voter
-	doctx.Option = msg.Option.String()
+	doctx.Option = int32(msg.Option)
 	doctx.ProposalID = msg.ProposalId
 }
 
